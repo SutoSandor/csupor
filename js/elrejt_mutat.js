35a5ae083b15f1899a828d6e216767b83094aad1
SutoSandor <sandorsuto97@gmail.com>
2018-11-29 14:19:34 +0100

    Initial commit

---
+++ js/elrejt_mutat.js
@@ -0,0 +1,137 @@
+function mutat() {
+    var x = document.getElementById("egy");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat2() {
+    var x = document.getElementById("ketto");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat3() {
+    var x = document.getElementById("harom");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat4() {
+    var x = document.getElementById("negy");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat5() {
+    var x = document.getElementById("ot");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat6() {
+    var x = document.getElementById("hat");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat7() {
+    var x = document.getElementById("het");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat8() {
+    var x = document.getElementById("nyolc");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat9() {
+    var x = document.getElementById("kilenc");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat10() {
+    var x = document.getElementById("tiz");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat11() {
+    var x = document.getElementById("tegy");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat12() {
+    var x = document.getElementById("tketto");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat13() {
+    var x = document.getElementById("tharom");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat14() {
+    var x = document.getElementById("tnegy");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat15() {
+    var x = document.getElementById("tot");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat16() {
+    var x = document.getElementById("that");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+function mutat17() {
+    var x = document.getElementById("thet");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+f
